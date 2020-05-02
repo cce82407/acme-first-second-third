6eab12dec3049d20c0c343eec87837d5e17c513a
Cynthia E <cynthia8499@hotmail.com>
2020-05-01 23:14:04 -0400

    Homework 4/29

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const app = document.querySelector('#app')
 
 const state = { 
     customers : [
-    { id: 1, name: 'moe', slot: 'first'},
+    { id: 1, name: 'moe', slot: 'third'},
     { id: 2, name: 'larry', slot: 'second'},
     { id: 3, name: 'shep', slot: 'third', selected: true},
     { id: 4, name: 'lucy', slot: 'first', selected: true},
@@ -37,7 +37,6 @@ const createContainer = () =>{
     first.innerHTML = 'FIRST'
     first.append(fLeftButton, fRightButton)
     
-
     const second = create('div','secondBox')
     second.innerHTML = 'SECOND'
     second.append(sLeftButton, sRightButton)
@@ -45,14 +44,17 @@ const createContainer = () =>{
     const third = create('div','thirdBox')
     third.innerHTML = 'THIRD'
     third.append(tLeftButton, tRightButton)
-    container.append(first, second, third)
     
-    const createUser = ({name, slot, selected}) => {
+    container.append(first, second, third)
 
-        //d ='${ customer.id }' ${customer.isVIP  ? "class = 'vip'": ''}'>
+    
 
+   
 
+    const createUser = ({id, name, slot, selected}) => {
         const user = create('div', selected  ? 'selected':'notSelected')
+        user.setAttribute('id', id)
+        user.classList.add('users')
         user.innerHTML = name
         if (slot === 'first'){
             first.append(user)
@@ -63,21 +65,42 @@ const createContainer = () =>{
         if (slot === 'third'){
             third.append(user)
         }
-    }
+        user.addEventListener('click', (ev) => {
+            const stateUser = state.customers[ev.target.id -1]
+            //console.log(typeof ev.target.id)
+            //console.log(typeof state.customers[ev.target.id -1].id)
+            //if (ev.target.id === strUserId){
+                stateUser.selected = !stateUser.selected     
+        
+
+
+            render()})  
+        }            
+
     
+        tLeftButton.addEventListener('click', (ev) => {
+            const thisUser = third.getElementsByClassName('users')
+            
+            const test = [...thisUser][0]
+            console.log(test)
+        })    
+        
+    
+    
+    
+    
+
     state.customers.map(user => createUser(user))
+    //console.log(state.customers)
     return container
 }
 
 
-    
-
-
 
 const render = () => {
+    app.innerHTML = ''
     const title = create('h1')
     title.innerText = "Acme First, Second, Third"
     app.append(title, createContainer())
-    
 }
 render()
